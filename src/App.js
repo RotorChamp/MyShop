f15808cb58119b3f69984d096439d194c120bfab
RotorChamp <152025503+RotorChamp@users.noreply.github.com>
2024-07-03 13:53:42 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+class App extends React.Component() {
+  constructor(props) {
+    super(props)
+    this.state
+  }
+  render() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+}
+export default App;
